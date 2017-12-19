0c76f199c38680f2872d5ac321c74cae50d981f6
unknown <kang.yan@ucloudlink.com>
2017-12-19 19:44:26 +0800

    1219

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var main = {};
+
+cqsj.initShareInfo = function () {
+    var currUrl = cqsj.url.replace(window.location.hash, '');
+    $.getJSON('http://www.sphinxcapital.com.cn/wxapi/signature.php?url=' + encodeURIComponent(currUrl)).done(function (data) {
+        wx.config({
+            debug: false,
+            appId: data.appId,
+            timestamp: data.timestamp,
+            nonceStr: data.nonceStr,
+            signature: data.signature,
+            jsApiList: [
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ]
+        });
+    });
+
+    wx.ready(function () {
+        // 微信分享
+        // 分享给朋友
+        wx.onMenuShareAppMessage({
+            title: '', // 分享标题
+            desc: '', // 分享描述
+            link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '' // 分享图标
+        });
+        // 分享到朋友圈
+        wx.onMenuShareTimeline({
+            title: '', // 分享标题
+            link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+        });
+    });
+}
