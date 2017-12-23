3031ebd6fe93a08347c8bd17b8dadc8c71db82c2
cyan <cyanyiyi@foxmail.com>
2017-12-23 16:42:27 +0800

    share

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 var main = {};
-
+main.init = function() {
+    // this.initShareInfo();
+    main.bindEvent();
+}
 main.initShareInfo = function () {
     var currUrl = cqsj.url.replace(window.location.hash, '');
     $.getJSON('http://www.sphinxcapital.com.cn/wxapi/signature.php?url=' + encodeURIComponent(currUrl)).done(function (data) {
@@ -33,3 +36,95 @@ main.initShareInfo = function () {
         });
     });
 }
+
+main.initUserinfo = function(nickName, avatarUrl) {
+    $('.t-userinfo-avatar img').attr("src", avatarUrl);
+    $('.t-userinfo-nickname').text(nickName);
+
+}
+
+main.generateCode = function(url) {
+    console.log(url);
+    var codeEl = $('#share-code')[0];
+    var width = codeEl.offsetWidth;
+    var height = codeEl.offsetHeight;
+    var qrcode = new QRCode(codeEl, {
+        width : width,
+        height : height
+    });
+    qrcode.makeCode(url);
+}
+
+main.takeScreenshot = function() {
+    var cntElem = $('#generatePic')[0];
+    var shareContent = cntElem;//需要截图的包裹的（原生的）DOM 对象
+    var width = shareContent.offsetWidth; //获取dom 宽度
+    var height = shareContent.offsetHeight; //获取dom 高度
+    var canvas = document.createElement("canvas"); //创建一个canvas节点
+    var scale = 3; //定义任意放大倍数 支持小数
+    canvas.width = width * scale; //定义canvas 宽度 * 缩放
+    canvas.height = height * scale; //定义canvas高度 *缩放
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+    canvas.getContext("2d").scale(scale, scale); //获取context,设置scale 
+    html2canvas($("#generatePic"), {
+        canvas: canvas,
+        onrendered: function (canvas) {
+            console.log(canvas);
+            // var url = canvas.toDataURL();
+            $('#generatePicShow').append(canvas).show();
+        }
+    });
+}
+
+main.sort = function (ARR) {
+    ARR.sort(function () {
+        return 0.5 - Math.random()
+    });
+    return ARR;
+};
+
+main.Randomwish = function ($domArr, target) {
+    var _target = main.sort(target);
+    console.log(_target);
+    for (var i = 0; i < $domArr.length; i++) {
+        (function(i){
+            $($domArr[i]).text(_target[i])
+        })(i)
+    }
+
+};
+
+main.bindEvent = function() {
+    // 选择系统目标
+    $(".target-text").click(function () {
+        $('#input-target').val($(this).attr('target-text'));
+    })
+    // 生成我的目标海报
+    $("#submit-target").on("click", function () {      
+        var myTarget = $('#input-target').val();
+        var _targetArr = ['目标1','目标2','目标3','目标4','目标5','目标6','目标7','目标8','目标9'];
+        var $domArr = $('.t-nine-text');
+        console.log(myTarget);
+        if(!myTarget){
+            $('.one-input').addClass('bounce').find('#input-target').addClass('red-input');
+            setTimeout(function(){
+                $('.one-input').removeClass('bounce').find('#input-target').removeClass('red-input');
+            },2000)
+        } else {
+            $('#generatePic').show();
+            if (_targetArr.indexOf(myTarget) === -1) {
+                _targetArr[5] = myTarget;
+                console.log(2222)
+            }
+            var nickName = '柳岩';
+            var avatarUrl = './img/ly2.jpg';
+            main.initUserinfo(nickName, avatarUrl);
+            main.Randomwish($domArr, _targetArr);
+            main.generateCode('www.tx.com?id=1');         
+            main.takeScreenshot();
+            $('#home').hide();
+        }
+    });
+}
+main.init();
